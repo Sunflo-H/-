f03662f4c9d3ab9dfa0cd406cc1e4ebbcb5e449a
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-10-21 18:13:34 +0900

    컨텍스트 메뉴 : o 클릭한 위치 주위를 원형으로   둘러싼 메뉴 생성 o 메뉴 개수에 따른 메뉴들 위치 o 그 메뉴들의 컨텐츠가   정상각도로 보이게 하기 o 클릭후 메뉴가 시작되는 지점 o 클릭후 메뉴가 생성될 거리 o 메뉴안에 이미지 or 세권정보 o 메뉴 나타날때 애니메이션

---
+++ component/contextMenu/contextMenu.js
@@ -0,0 +1,51 @@
+const contextMenuWrapper = document.querySelector(".context-menu-wrapper");
+const li = document.querySelectorAll("li");
+const contextMenu = document.querySelector(".context-menu");
+
+// 메뉴의 개수에 따른 각도
+let option = {
+  six: 60,
+  eight: 45
+};
+
+function hideMenu() {
+  li.forEach((item, index) => {
+    item.style.opacity = 1;
+    item.style.transform = `rotate(0deg) translate(0px)`;
+  });
+  contextMenuWrapper.style.opacity = 0;
+  contextMenu.style.opacity = 0;
+}
+
+function rightClick(e) {
+  e.preventDefault();
+  if (contextMenuWrapper.style.opacity == 1) {
+    hideMenu();
+  } else {
+    contextMenuWrapper.style.opacity = 1;
+    contextMenuWrapper.style.left = e.pageX - 25 + "px";
+    contextMenuWrapper.style.top = e.pageY - 40 + "px";
+    contextMenu.style.opacity = 1;
+
+    li.forEach((item, index) => {
+      const cover = item.querySelector(".context-menu_cover");
+      item.style.opacity = 1;
+
+      // 클릭 지점에서부터 메뉴의 거리와 각도를 조절
+      item.style.transform = `rotate(${
+        index * option.eight
+      }deg) translate(-90px)`;
+
+      // 달라진 메뉴의 각도만큼 안의 컨텐츠의 각도를 반대로 돌려 정상각도로 만듬
+      cover.style.transform = `rotate(-${index * option.eight}deg)`;
+    });
+  }
+}
+
+document.addEventListener("click", (e) => {
+  hideMenu();
+});
+
+document.addEventListener("contextmenu", (e) => {
+  rightClick(e);
+});
