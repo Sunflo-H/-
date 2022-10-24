385920922e9fd02713f3b070b9fee39d2ea5966e
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-10-24 18:16:21 +0900

    html, css : 헤더에 쉐도우 없애고, nav item의 border-bottom이 위로오게, 지도 표현, main과 header, contextMenu와 합치면서 생기는 문제 해결 및 다듬기

--- src/js/contextMenu.js
+++ src/js/contextMenu.js
@@ -24,15 +24,17 @@ function rightClick(e) {
   } else {
     contextMenuWrapper.style.opacity = 1;
     contextMenuWrapper.style.left = e.pageX - 25 + "px";
-    contextMenuWrapper.style.top = e.pageY - 40 + "px";
+    contextMenuWrapper.style.top = e.pageY - 40 - 70 + "px";
     contextMenu.style.opacity = 1;
 
+    console.log(contextMenuWrapper.style.top);
+    console.log(e);
     li.forEach((item, index) => {
       const cover = item.querySelector(".context-menu_cover");
       item.style.opacity = 1;
 
       // 클릭 지점에서부터 메뉴의 거리와 각도를 조절
-      item.style.transform = `rotate(${index * option.eight}deg) translate(-90px)`;
+      item.style.transform = `rotate(${index * option.eight}deg) translate(-80px)`;
 
       // 달라진 메뉴의 각도만큼 안의 컨텐츠의 각도를 반대로 돌려 정상각도로 만듬
       cover.style.transform = `rotate(-${index * option.eight}deg)`;
