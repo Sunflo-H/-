0770d375482fe5a8841ed9d6c9a22190de565072
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-11-02 19:17:03 +0900

    클러스터 적용

--- component/main/aptFilter.js
+++ component/main/aptFilter.js
@@ -1,32 +1,15 @@
-const filter = document.querySelector('.filter');
+const filter = document.querySelector(".filter");
 const size = filter;
-const sizeSelect = size.querySelector('.filter__select');
-const sizeOption = size.querySelector('.filter__option-table');
-const sizeOptionItem = size.querySelectorAll('.filter__option');
+const sizeSelect = size.querySelector(".filter__select");
+const sizeOption = size.querySelector(".filter__option-table");
+const sizeOptionItem = size.querySelectorAll(".filter__option");
 
-const slider = filter.querySelector('.filter__slider-bottom input');
+const slider = filter.querySelector(".filter__slider-bottom input");
 
-console.log(slider);
-sizeSelect.addEventListener('click', e=> {
-    sizeOption.style.display = "block";
-})
+sizeSelect.addEventListener("click", (e) => {
+  sizeOption.style.display = "block";
+});
 
-
-
-document.addEventListener('click', e=> {
-    if(e.target !== sizeSelect) sizeOption.style.display = "none";
-
-})
-
-// function ShowSliderValue(sVal){
-//     var obValueView = document.getElementById("slider_value_view");
-//         obValueView.innerHTML = sVal
-// }
-// var RangeSlider = function(){
-//     var range = $('.slider_range');
-//     range.on('input', function(){
-//         ShowSliderValue(this.value);
-//     });
-// };
-// RangeSlider();
-
+document.addEventListener("click", (e) => {
+  if (e.target !== sizeSelect) sizeOption.style.display = "none";
+});
