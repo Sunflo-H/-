cbf759b776e3bc57cc13f41111bc4c4cd358bb40
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-11-25 07:39:46 +0900

    js : 안쓰는 변수 정리 및 변수,함수 설명 추가

--- component/main/main.js
+++ component/main/main.js
@@ -21,11 +21,24 @@ const local = [
 ];
 const localOverlayList = [];
 const subwayOverlayList = [];
+/**
+ * 생성된 클러스터 객체 : _clusters, _markers 등의 여러 정보를 가지고있다.
+ */
 let roomCluster = null;
-let roomClusterState = false; //true면 지도 레벨이 낮을때 roomCluster만 보이게된다, //false면 지도 레벨이 낮을때 subwayOverlay만 보이게된다.
+/**
+ * roomCluster 존재여부를 확인하는 변수,
+ * roomCluster는 promise데이터로 만들기때문에 지연되는 시간차를 막고자 따로 만들었다.
+ */
+let roomClusterState = false;
+/**
+ * 클러스터를 생성할때 마커와 방정보를 함께 매핑한 배열
+ */
 let roomAndMarker = null;
-let markers = null;
-let oneroomList_resolved = null;
+/**
+ * 현재 카드리스트에 사용되는 방 정보를 저장한 배열,
+ * 줌 인, 아웃 후 다시 똑같은 카드를 만들때 사용된다.
+ */
+let currentOneroomList = null;
 
 /**
  * !설명
@@ -36,65 +49,21 @@ let oneroomList_resolved = null;
  * 2. 지하철 오버레이를 클릭하면 방 클러스터가 생성된다.
  * 3. 방을 리스트로 보여주며, 모든 방에 대해 필터적용, 각 방에 대한 세권찾기
  */
-//* 필터, 세권 만들기
+
+//! 할것
 //* 매물 정렬에 가격 => 보증금순, 월세순 으로 바꾸고, 월세순으로 할때 전세는 월세가 없으니까 맨 아래로 보내
+// 1 현재 데이터가 cardlist에 어떻게 제공되는지 확인하기
+// 2 데이터에서 보증금, 월세, 최신 에 대한 데이터를 확인 후 비교할수 있는지 생각하기
+
+//* 클릭한 지점의 클러스터 오버레이의 css 변화
+//* 필터, 세권 만들기
+//* 처음에 자기 위치 받아와서 바로 지하철로 보이게 만들기
 
 // 지도 생성
 const map = new kakao.maps.Map(document.getElementById("map"), {
   center: new kakao.maps.LatLng(37.53886742395844, 126.98678427911392),
   level: 8,
   maxLevel: 12,
-});
-
-kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-  // console.log(mouseEvent.latLng);
-  console.log(map.getLevel());
-  console.log(roomCluster);
-});
-
-// ! 오버레이에 이벤트를 등록해야하는 상황
-// !1.에서 처음부터 생성.. 사용자 위치를 기준으로 지도레벨 확대해서 지하철부터 보여줄까???????
-// 1. 새 오버레이 생성 (로컬의 경우 처음부터 생성되었기때문에 이때 적용함, subway랑 매물은 아님)
-// 2. 지도확대 축소후 새로 생긴 오버레이
-// 3. 지도 이동후 새로생긴 오버레이
-// ^ 지도의 드래그가 끝났을때 화면에 보여지는 오버레이에 이벤트 등록
-kakao.maps.event.addListener(map, "dragend", function () {
-  overlaySetEvent();
-});
-let resultMarkerList = [];
-
-/**
- * ^ 지도 레벨에 따라 지역, 지하철, 방을 보여준다.
- * ^ 보여지는 오버레이에 클릭이벤트를 등록한다.
- */
-kakao.maps.event.addListener(map, "zoom_changed", function (mouseEvent) {
-  // 5이하 : 매물, 6~8 : 지하철, 9이상 : 지역
-  if (5 < map.getLevel() && map.getLevel() < 8) {
-    console.log("지하철 오버레이");
-    localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
-    subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(map));
-    displayRoomCluster(false);
-    createCardList();
-  } else if (map.getLevel() <= 5) {
-    if (roomClusterState) {
-      console.log("줌이 바뀌었는데 방정보 있을때");
-      displayRoomCluster(true);
-      createCardList(oneroomList_resolved);
-      localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
-      subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(null));
-    } else {
-      console.log("줌이 바뀌었는데 방정보 없을때");
-      localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
-      subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(map));
-    }
-  } else {
-    console.log("로컬 오버레이 보여줘");
-    localOverlayList.forEach((localOverlay) => localOverlay.setMap(map));
-    subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(null));
-    createCardList();
-  }
-
-  overlaySetEvent();
 });
 
 //* ============================== 방 정보, 방 클러스터 관련 함수 =================================
@@ -109,8 +78,6 @@ async function getOneRoomCluster(subway) {
   // oneroomList를 foreach 돌려서 async 안에 async의 흐름 확인해보기
   Promise.all(oneroomList).then((oneroomList) => {
     createCluster(oneroomList);
-    // createCardList(oneroomList);
-    oneroomList_resolved = oneroomList;
     loading(false);
   });
 }
@@ -133,8 +100,8 @@ function createCardList(oneroomList = null) {
     cards.removeChild(cards.firstChild);
   }
 
+  // oneroomList가 없다면 기본값을 띄운다.
   if (oneroomList === null) {
-    console.log("널입니다.");
     let element = `<li class="no-result">
                     <p class="no-result__text"><b>장소</b>를 클릭하여</p>
                     <p class="no-result__text">매물을 확인해보세요.</p>
@@ -142,8 +109,9 @@ function createCardList(oneroomList = null) {
     cards.insertAdjacentHTML("beforeend", element);
     return;
   }
-  console.log("실행");
-  oneroomList_resolved = oneroomList;
+
+  currentOneroomList = oneroomList;
+
   oneroomList.forEach((oneroom) => {
     let item = oneroom.item;
     let price = ``;
@@ -222,7 +190,7 @@ function createCluster(roomList) {
     calculator: [10],
   });
   roomAndMarker = [];
-  markers = roomList.map(function (room, i) {
+  let markers = roomList.map(function (room, i) {
     let position = room.item.random_location.split(",");
     let marker = new kakao.maps.Marker({
       position: new kakao.maps.LatLng(position[0], position[1]),
@@ -242,13 +210,7 @@ function createCluster(roomList) {
   roomCluster.addMarkers(markers);
 
   // * 생성된 클러스터에 적용하는 이벤트들
-  // & clustered(클러스터 생성 완료 후) 이벤트가 바로 적용되지 않는 이유 :
-  // &   클러스터가 생성된 후에 클러스터들에게 이벤트를 적용시킨다.
-  // &   생성된 후에 clustered 이벤트를 적용시킨다.
-  // &   따라서 처음 생성된 클러스터에는 clustered 이벤트가 적용되지 않는다.
-  // &   그리고 생성된 클러스터는 지도가 확대/축소될때마다 clustered되며 이때마다 이벤트가 적용된다.
-
-  // 처음 생성된 클러스터에 적용하는 이벤트 (clustered 이벤트핸들러와 내용은 같다.)
+  // 처음 생성된 클러스터에 적용하는 이벤트 (clustered 이벤트핸들러와 기능은 같다.)
   roomCluster._clusters.forEach((cluster) => {
     let overlay = cluster.getClusterMarker().getContent();
     overlay.addEventListener("mouseover", function () {
@@ -263,21 +225,10 @@ function createCluster(roomList) {
     });
   });
 
-  kakao.maps.event.addListener(roomCluster, "clusterclick", function (cluster) {
-    let roomList = cluster
-      .getMarkers()
-      .map(
-        (marker) =>
-          roomAndMarker.find((item) => marker === item.marker).roomData
-      );
-
-    createCardList(roomList);
-  });
-
   kakao.maps.event.addListener(roomCluster, "clustered", function (clusters) {
-    for (var i = 0; i < clusters.length; i++) {
-      var cluster = clusters[i];
-      var overlay = cluster.getClusterMarker().getContent();
+    for (let i = 0; i < clusters.length; i++) {
+      let cluster = clusters[i];
+      let overlay = cluster.getClusterMarker().getContent();
 
       overlay.addEventListener("mouseover", function () {
         if (!this.classList.contains("cluster-over")) {
@@ -291,6 +242,17 @@ function createCluster(roomList) {
         }
       });
     }
+  });
+
+  kakao.maps.event.addListener(roomCluster, "clusterclick", function (cluster) {
+    let roomList = cluster
+      .getMarkers()
+      .map(
+        (marker) =>
+          roomAndMarker.find((item) => marker === item.marker).roomData
+      );
+
+    createCardList(roomList);
   });
 }
 
@@ -420,12 +382,14 @@ function subwayOverlayClick(event) {
   );
 }
 
+/**
+ * ^ 지역, 지하철 overlay들에게 클릭이벤트를 등록하는 함수
+ */
 function overlaySetEvent() {
   const localOverlay = document.querySelectorAll(
     ".customOverlay.customOverlay--local"
   );
   localOverlay.forEach((overlay) => {
-    console.log("count");
     overlay.addEventListener("click", localOverlayClick);
   });
 
@@ -441,21 +405,86 @@ function overlaySetEvent() {
   }, 500);
 }
 
+/**
+ * ^ 로딩바가 보이게 하는 함수
+ * @param {*} boolean ture:보인다, false:사라진다.
+ */
 function loading(boolean) {
   const loading = document.querySelector("#loading");
   if (boolean) loading.classList.add("active");
   else loading.classList.remove("active");
 }
 
+/**
+ * ^ 평수를 구하는 함수
+ * @param {*} size 전용면적
+ * @returns 평수
+ */
 function getPyeong(size) {
   return Math.floor(Math.round(Number(size)) / 3);
 }
 
+/**
+ * ^ 프로그램 시작시 실행되는 초기화 함수
+ * ^ 처음부터 실행되어야할 함수들을 모았다.
+ */
 function init() {
   createOverlay_subway_all();
   createOverlay_local(local);
-  createCardList(null);
+  createCardList();
   // setCardList();
 }
 
 init();
+
+//* ================================================== 이벤트들 ===============================================================
+kakao.maps.event.addListener(map, "click", function (mouseEvent) {
+  // console.log(mouseEvent.latLng);
+  console.log(map.getLevel());
+  console.log(roomCluster);
+});
+
+// ! 오버레이에 이벤트를 등록해야하는 상황
+// !1.에서 처음부터 생성.. 사용자 위치를 기준으로 지도레벨 확대해서 지하철부터 보여줄까???????
+// 1. 새 오버레이 생성 (로컬의 경우 처음부터 생성되었기때문에 이때 적용함, subway랑 매물은 아님)
+// 2. 지도확대 축소후 새로 생긴 오버레이
+// 3. 지도 이동후 새로생긴 오버레이
+
+// 지도의 드래그가 끝났을때 화면에 보여지는 오버레이에 이벤트 등록
+kakao.maps.event.addListener(map, "dragend", function () {
+  overlaySetEvent();
+});
+
+/**
+ * 지도 레벨에 따라 지역, 지하철, 방을 보여준다.
+ * 보여지는 오버레이에 클릭이벤트를 등록한다.
+ */
+kakao.maps.event.addListener(map, "zoom_changed", function (mouseEvent) {
+  // 5이하 : 매물, 6~8 : 지하철, 9이상 : 지역
+  if (5 < map.getLevel() && map.getLevel() < 8) {
+    // console.log("지하철 오버레이를 띄워야할때");
+    localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
+    subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(map));
+    displayRoomCluster(false);
+    createCardList();
+  } else if (map.getLevel() <= 5) {
+    if (roomClusterState) {
+      // console.log("줌이 바뀌었는데 방정보 있을때");
+      displayRoomCluster(true);
+      createCardList(currentOneroomList);
+      localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
+      subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(null));
+    } else {
+      // console.log("줌이 바뀌었는데 방정보 없을때");
+      localOverlayList.forEach((localOverlay) => localOverlay.setMap(null));
+      subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(map));
+    }
+  } else {
+    // console.log("로컬 오버레이를 띄워야할때");
+    localOverlayList.forEach((localOverlay) => localOverlay.setMap(map));
+    subwayOverlayList.forEach((subwayOverlay) => subwayOverlay.setMap(null));
+    createCardList();
+  }
+
+  overlaySetEvent();
+});
